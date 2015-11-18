d67bd44411b88ec0e2a6f1e8a5fd56617cb35bed
juan cabrera <juancabrera.dev@gmail.com>
2015-11-18 20:03:16 +0100

    Command Pattern

---
+++ src/org/java/behave/command/CommandDemo.java
@@ -0,0 +1,20 @@
+package org.java.behave.command;
+
+public class CommandDemo {
+
+	public static void main(String[] args) {
+		
+		Stock abcStock = new Stock();
+		
+		BuyStock buyStockOrder = new BuyStock(abcStock);
+		SellStock sellStockOrder = new SellStock(abcStock);
+		
+		Broker broker = new Broker();
+		broker.takeOrder(buyStockOrder);
+		broker.takeOrder(sellStockOrder);
+		
+		broker.placeOrders();
+
+	}
+
+}
---
+++ src/org/java/behave/command/Order.java
@@ -0,0 +1,5 @@
+package org.java.behave.command;
+
+public interface Order {
+	void execute();
+}
