f3306af93db9e21db254241a63cc7a8073eaed4a
JuanCabrera <juancabrera.it@gmail.com>
2015-11-18 14:54:09 +0100

    Composite Employee fix

--- src/org/java/stuct/composite/Employee.java
+++ src/org/java/stuct/composite/Employee.java
@@ -10,7 +10,7 @@ public class Employee {
 	private int salary;
 	private List<Employee> subordinates;
 	
-	public Employee(String name, String dept, int sal) {
+	public Employee(String name, String dept, int salary) {
 	
 		this.name = name;
 		this.dept = dept;
